7a343701878b923550322e4ab365c176d7153e12
Uhan7 <luckyfellowfella@gmail.com>
2026-09-09 23:02:28 +0800

    green circle

---
+++ build/CMakeFiles/green_circle.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/green_circle.dir/main.cpp.o"
+  "CMakeFiles/green_circle.dir/main.cpp.o.d"
+  "green_circle"
+  "green_circle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/green_circle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
