4b5ffbae748e8fdadc597d58f81683339fcead17
Uhan7 <luckyfellowfella@gmail.com>
2026-09-14 14:14:09 +0800

    DONE

--- build-new/CMakeFiles/Makefile.cmake
+++ build-new/CMakeFiles/Makefile.cmake
@@ -56,5 +56,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/green_circle.dir/DependInfo.cmake"
+  "CMakeFiles/app.dir/DependInfo.cmake"
   )
--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <SFML/Graphics.hpp>
 #include <string>
+#include <iomanip>
+#include <sstream>
+#include <vector>
 
 // Prototypes
 unsigned int GetFrameRate();
 
 // Constants
-const unsigned int WINDOW_WIDTH = 1920;
-const unsigned int WINDOW_HEIGHT = 1080;
+const int WINDOW_WIDTH = 1920;
+const int WINDOW_HEIGHT = 1080;
+const unsigned int ICON_COUNT = 480;
+const unsigned int ICON_SIZE = 64;
+const unsigned int ICON_COLUMNS = 30; // 1920 (window width) / 64 (icon width) = 30
 
 int main()
 {
@@ -15,25 +21,65 @@ int main()
 
     // Textures
     sf::Texture desertTexture("Media/Textures/Desert.png");
+    std::vector<sf::Texture> iconTextures;
 
     // Fonts
     sf::Font font("Media/Sansation.ttf");
 
     // Sprites
-    sf::Sprite desertBackground(desertTexture);
+    sf::Sprite desertBackground1(desertTexture);
+    sf::Sprite desertBackground2(desertTexture);
+    std::vector<sf::Sprite> iconSprites;
 
     // Texts
     sf::Text fpsText(font, "Hello World", 36);
     fpsText.setFillColor(sf::Color::White);
-    fpsText.setPosition({WINDOW_WIDTH - 600.f, WINDOW_HEIGHT - 300.f});
+    fpsText.setPosition({20, 20});
     
-    // Initializing
+    // Variables
     unsigned int frameCount = 0;
+    float backgroundMovementSpeed = 0.25f;
+
+    // Initializing Stuff
+    desertBackground1.setPosition({0, 0});
+    desertBackground2.setPosition({0, static_cast<float>(WINDOW_HEIGHT)});
+
+    // Icon Textures
+    for (unsigned int i = 0; i < ICON_COUNT; i++)
+    {
+        std::ostringstream filename;
+        filename << "Media/Streaming/tile" << std::setw(3) << std::setfill('0') << i << ".png";
+        iconTextures.emplace_back(filename.str());
+    }
+
+    // Icon Sprites
+    for (unsigned int i = 0; i < ICON_COUNT; i++)
+    {
+        iconSprites.emplace_back(iconTextures[i]);
+
+        const unsigned int column = i % ICON_COLUMNS;
+        const unsigned int row = i / ICON_COLUMNS;
+
+        iconSprites.back().setPosition({static_cast<float>(column * ICON_SIZE), static_cast<float>(row * ICON_SIZE)});
+    }
+
+    // Background Resizing
+    const sf::Vector2u backgroundSize = desertTexture.getSize();
+
+    desertBackground1.setScale({
+        static_cast<float>(WINDOW_WIDTH) / static_cast<float>(backgroundSize.x),
+        static_cast<float>(WINDOW_HEIGHT) / static_cast<float>(backgroundSize.y)
+    });
+
+    desertBackground2.setScale({
+        static_cast<float>(WINDOW_WIDTH) / static_cast<float>(backgroundSize.x),
+        static_cast<float>(WINDOW_HEIGHT) / static_cast<float>(backgroundSize.y)
+    });
 
     // Main Loop
     while (window.isOpen())
     {
-        // Event Loop
+        // Events Loop
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>()) window.close();
@@ -41,9 +87,16 @@ int main()
 
         // Updating
         fpsText.setString("FPS: " + std::to_string(GetFrameRate()));
+        desertBackground1.move({0, -backgroundMovementSpeed});
+        desertBackground2.move({0, -backgroundMovementSpeed});
+
+        if (desertBackground1.getPosition().y <= -WINDOW_HEIGHT) desertBackground1.setPosition({0, static_cast<float>(WINDOW_HEIGHT)});
+        if (desertBackground2.getPosition().y <= -WINDOW_HEIGHT) desertBackground2.setPosition({0, static_cast<float>(WINDOW_HEIGHT)});
 
         // Rendering
-        window.draw(desertBackground);
+        window.draw(desertBackground1);
+        window.draw(desertBackground2);
+        for (const sf::Sprite& icon : iconSprites) window.draw(icon);
         window.draw(fpsText);
 
         // Refresh
